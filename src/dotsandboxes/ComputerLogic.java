8905de2376d8819e43b4629f78e56a6570ab8df9
D1k7u5 <diktus@gmx.ch>
2015-05-15 13:05:26 +0200

    LineDetection in GPanel integriert, getestet und Fehler behoben

---
+++ src/dotsandboxes/ComputerLogic.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dotsandboxes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author davidheer
+ */
+public class ComputerLogic {
+    
+    ArrayList<Box> box0Line = new ArrayList<>();
+    ArrayList<Box> box1Line = new ArrayList<>();
+    ArrayList<Box> box2Line = new ArrayList<>();
+    ArrayList<Box> box3Line = new ArrayList<>();
+    ArrayList<ArrayList> twoLineTube = new ArrayList<>(); //Zusammenhängende Boxen mit zwei Linien, zu offenen Linien
+
+    public ComputerLogic(){
+        
+    }
+
+    
+    /**
+     * Sortiert Boxen nach anzahl Linien in ArrayLists
+     */
+    private void sortBoxes(){
+        for(Box box : box0Line){
+            int setedLines;
+            setedLines = box.getNumberOfSetedLines();
+            
+            if(setedLines == 0){box0Line.add(box);}
+            if(setedLines == 1){box1Line.add(box);}
+            if(setedLines == 2){box2Line.add(box);}
+            if(setedLines == 3){box3Line.add(box);}
+        }
+    }
+    
+    private void check3LineBoxes(){
+        
+    }
+    
+    private void check0Lines(){
+        
+    }
+    
+    private void check1Line(){
+        
+    }
+    
+    /**
+     * Prüfe Boxen mit zwei Linien und erstelle
+     */
+    private void check2Line(){
+        /*
+        
+        for(Box box : box2Line){
+            if(box.getLineN() == null){
+                //Prüfe Box im Norden
+            }
+            if(box.getLineE() == null){
+                twoLineTube.
+                //Prüfe Box im Osten
+            }
+        }
+        */
+    }
+    
+}
